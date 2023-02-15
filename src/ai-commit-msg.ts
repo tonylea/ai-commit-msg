12e020d2ba91d770e5e69135a5e05e3449221e6c
Tony Lea <tony.lea@vanquisbank.co.uk>
2023-02-15 08:50:04 +0000

    chore: add clear dependency and call it in aicommitmsg function

--- src/ai-commit-msg.ts
+++ src/ai-commit-msg.ts
@@ -1,6 +1,10 @@
 import { showTitleAndBanner } from "./utils/logger.util";
 
+const clear = require("clear");
+
 export async function AiCommitMsg(): Promise<any> {
+  clear();
+
   showTitleAndBanner();
 
   console.log(`COMMIT-MSG::::: `);
